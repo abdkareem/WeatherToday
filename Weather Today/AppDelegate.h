ae8dcf8ff6b1d03d2813b79e8b85f3f98079265b
AbdulKareem KhajaMohammed <abd.khaja@gmail.com>
2016-05-02 22:08:10 -0700

    Initial Commit

---
+++ Weather Today/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Weather Today
+//
+//  Created by Taiyaba Sultana on 5/2/16.
+//  Copyright © 2016 Abdul Kareem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Weather Today/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weather Today
+//
+//  Created by Taiyaba Sultana on 5/2/16.
+//  Copyright © 2016 Abdul Kareem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
